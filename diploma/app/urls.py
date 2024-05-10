2f935e7d1e1c258b0143b3ca371a14328eb89ed9
salivan88 <iaroslav.galchyshak.03@gmail.com>
2024-05-10 21:59:39 +0300

    basically set up authentication

--- diploma/app/urls.py
+++ diploma/app/urls.py
@@ -4,5 +4,14 @@ from app.views import *
 urlpatterns = [
     path('', index),
     path('profile', profile),
-    path('watched', watched)
+    path('watched', watched),
+
+
+    path('register', register, name="register"),
+
+    path('my-login', my_login, name="my-login"),
+
+    path('dashboard', dashboard, name="dashboard"),
+
+    path('user-logout', user_logout, name="user-logout"),
 ]
