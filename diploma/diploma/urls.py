18f7082df87b39c417c4a69343341234141020fb
salivan88 <iaroslav.galchyshak.03@gmail.com>
2024-05-10 19:20:53 +0300

    upd "quick rec page"

--- diploma/diploma/urls.py
+++ diploma/diploma/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),    
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
